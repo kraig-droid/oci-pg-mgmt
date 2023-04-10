0dd70e316accda283894cd20e65159c854ded30c
Kraig Hanson <kraig@hbhome.org>
2023-04-10 04:19:51 +0000

    major rework

--- tf/instance.tf
+++ tf/instance.tf
@@ -1,7 +1,12 @@
+resource "tls_private_key" "this" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
 resource "oci_core_instance" "instance" { # The second string is a local name
     # Required (per oracle)
     availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
-    compartment_id = local.tenancy_ocid
+    compartment_id = "${var.tenancy_ocid}"
     shape = "VM.Standard.A1.Flex"
     shape_config {
         memory_in_gbs = 6
@@ -13,7 +18,7 @@ resource "oci_core_instance" "instance" { # The second string is a local name
     }
 
     # Optional
-    display_name = "p0"
+    display_name = "${var.instance_name}"
     create_vnic_details {
         assign_public_ip = true
         subnet_id = "ocid1.subnet.oc1.us-sanjose-1.aaaaaaaajlg75jvkvhvuabf7aqc6bwgekxr3uuwoox5qi5hu6w7rtqcumzuq"
@@ -24,3 +29,17 @@ resource "oci_core_instance" "instance" { # The second string is a local name
     preserve_boot_volume = false
 }
 
+variable "instance_name" {}
+variable "desired_state" {}
+
+output "private-ip-for-compute-instance" {
+  value = var.desired_state == "present" ? oci_core_instance.instance.private_ip : null
+}
+
+output "public-ip-for-compute-instance" {
+  value = var.desired_state == "present" ? oci_core_instance.instance.public_ip : null
+}
+
+output "ssh_key_fingerprint" {
+  value = var.desired_state == "present" ? try(tls_private_key.this.public_key_fingerprint_sha256, "") : null
+}
